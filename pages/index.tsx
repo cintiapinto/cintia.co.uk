7b135ccb5811cb06425c325e92974a2b862c4826
Cintia Pinto <cintia.fortespinto@artprocessors.net>
2022-06-23 10:01:29 +1000

    bug: 🐛 fix background grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,8 @@ const Home: NextPage = () => {
         <title>Cintia</title>
         <meta name="description" content="cintia.co.uk" />
       </Head>
-      <body>
-        <h1>Cintia Fortes Pinto is a frontend developer based in Melbourne.</h1>
-        <h4>Relocating to London in September 2022.</h4>
-      </body>
+      <h1>Cintia Fortes Pinto is a frontend developer based in Melbourne.</h1>
+      <h4>Relocating to London in September 2022.</h4>
     </div>
   )
 }
